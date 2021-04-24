e0c0d9e5ab8e84260b257b25920f9192d1d6bf88
Stanley Rokita <srok@Stanleys-MBP.lan>
2021-04-24 16:57:36 -0400

    Remove references section and update courses

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,5 +1,28 @@
 const courses = [
-  // TODO: Add courses back in
+  {
+    title: 'Intro to Operating Systems',
+    number: 'EECS 482',
+    link: 'https://web.eecs.umich.edu/~harshavm/eecs482/',
+    univesity: 'Michigan',
+  },
+  {
+    title: 'Web Systems',
+    number: 'EECS 485',
+    link: 'https://web.eecs.umich.edu/~harshavm/eecs482/',
+    univesity: 'Michigan',
+  },
+  {
+    title: 'Computer Networking',
+    number: 'EECS 489',
+    link: 'https://web.eecs.umich.edu/~sugih/courses/eecs489/',
+    univesity: 'Michigan',
+  },
+  {
+    title: 'Database Systems',
+    number: 'EECS 484',
+    link: 'https://web.eecs.umich.edu/~mozafari/eecs484/',
+    univesity: 'Michigan',
+  },
 ];
 
 export default courses;
